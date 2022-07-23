d495a6292e2af40c6ff7a55a25ee984a2991206f
ahmedhosnypro <ahhosnyas@gmail.com>
2022-07-23 09:02:38 +0200

    X and Y

--- Linear Equations Solver/task/src/solver/Main.java
+++ Linear Equations Solver/task/src/solver/Main.java
@@ -7,12 +7,19 @@ public class Main {
 
     public static void main(String[] args) {
         Main main = new Main();
-        main.solve();
+        main.linearEquation();
     }
 
-    private void solve() {
+    private void linearEquation() {
         double a = in.nextDouble();
         double b = in.nextDouble();
-        System.out.println(b / a);
+        double c = in.nextDouble();
+        double d = in.nextDouble();
+        double e = in.nextDouble();
+        double f = in.nextDouble();
+
+        double y = (f - c * (d / a)) / (e - b * (d / a));
+        double x = (c - b * y) / a;
+        System.out.printf("%.5f %.5f", x, y);
     }
 }
